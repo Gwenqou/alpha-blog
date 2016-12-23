21a823f09e3096f2660fd68843eb527c53ab8ce9
Gwen <wenqingou@gmail.com>
2016-12-23 21:21:44 +0000

    Signup users from UI

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def new 
+    @user=User.new
+  end 
+
+  
+  def create
+    @user = User.new(user_params) 
+    if @user.save
+      flash[:success]="You have successfully signed up."
+      redirect_to articles_path
+    else 
+      render 'new'
+    end 
+  end 
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  private 
+    
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end 
+end 
